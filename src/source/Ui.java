937864f11c74577160dd140fd63cf1b6a88ab3b4
vivi00790 <vivi00791@gmail.com>
2017-04-23 03:05:50 +0800

    added header

--- src/source/Ui.java
+++ src/source/Ui.java
@@ -11,14 +11,26 @@ public class Ui {
 	/**
 	 * @throws NoSuchIDException
 	 * @throws NoSuchCommandException
+	 * default constructor
 	 */
 	public Ui() throws NoSuchIDException, NoSuchCommandException {
 	}
 
 	/**
-	 * @param id
-	 * @return true if the gradesystem contains that id 
-	 * @throws NoSuchIDException if the gradesystem NOT contains that id
+	 * @param iGradeSystemAdaptor the instance of grade system
+	 * 
+	 * @throws NoSuchIDException
+	 * @throws NoSuchCommandException
+	 * 
+	 */
+	public Ui(IGradeSystem iGradeSystemAdaptor) throws NoSuchIDException, NoSuchCommandException{
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param id expected id of student
+	 * @return ? the database contains id above : the database doesn't contains id above
+	 * @throws NoSuchIDException if the database NOT contains that id
 	 */
 	public boolean checkId(String id) throws NoSuchIDException {
 		return false;
@@ -26,12 +38,13 @@ public class Ui {
 
 	/**
 	 * @throws NoSuchCommandException if user input the wrong command
+	 * prompt all command to console
 	 */
 	public void promptCommand() throws NoSuchCommandException {
 	}
 
 	/**
-	 * prompt user input the id
+	 * prompt user id
 	 */
 	public void promptId() {
 	}
@@ -43,8 +56,8 @@ public class Ui {
 	}
 
 	/**
-	 * @param id
-	 * show welcome message
+	 * @param id expected id
+	 * show welcome message and id above
 	 */
 	public void showWelcomeMsg(String id) {
 	}
